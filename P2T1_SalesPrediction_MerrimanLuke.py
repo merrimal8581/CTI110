9f777b4f425249216346445032fc404ae0303ac3
merrimal8581 <36463946+merrimal8581@users.noreply.github.com>
2018-02-13 21:46:50 -0500

    Add files via upload

---
+++ P2T1_SalesPrediction_MerrimanLuke.py
@@ -0,0 +1,17 @@
+Python 3.6.4 (v3.6.4:d48eceb, Dec 19 2017, 06:04:45) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+======== RESTART: C:/Users/Nic/Documents/CTI 110/P2T1_MerrimanLuke.py ========
+>>> 
+======== RESTART: C:\Users\Nic\Documents\CTI 110\sales_prediction.py ========
+Enter the projected sales: 1000
+The profit is $ 230.0
+>>> 
+======== RESTART: C:\Users\Nic\Documents\CTI 110\sales_prediction.py ========
+Enter the projected sales: 1000
+The profit is $ 230.00
+>>> 
+======== RESTART: C:\Users\Nic\Documents\CTI 110\sales_prediction.py ========
+Enter the projected sales: 10000
+The profit is $ 2,300.00
+>>> 
